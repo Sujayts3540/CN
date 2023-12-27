360a52d5737a8a3ff1b8b0dbf4a75506329790db
SUJAY T S <102526036+Sujayts3540@users.noreply.github.com>
2023-12-27 20:42:04 +0530

    Update pg1.tcl

--- pg1.tcl
+++ pg1.tcl
@@ -1,36 +1,43 @@
-# Create a simple network topology
+# Create a new simulation object
 set ns [new Simulator]
 
-set node0 [$ns node]
-set node1 [$ns node]
+# Create two nodes
+set router [$ns node]
+set high_priority_host [$ns node]
+set low_priority_host [$ns node]
 
-$ns duplex-link $node0 $node1 10Mb 10ms DropTail
+# Create links between nodes
+$ns duplex-link $high_priority_host $router 10Mb 10ms DropTail
+$ns duplex-link $low_priority_host $router 5Mb 20ms DropTail
 
-# Enable QoS on the link
-$ns queue-limit $node0 $node1 10
-$ns queue-type $node0 $node1 DropTail
-$ns drop-tail-prio $node0 $node1 0
+# Set up high-priority traffic source and sink
+set high_priority_source [new Application/Traffic/FTP]
+$high_priority_source attach-agent $high_priority_host
+set high_priority_sink [new Agent/TCPSink]
+$ns attach-agent $high_priority_host $high_priority_sink
+$ns connect $high_priority_source $high_priority_sink
+$high_priority_source set type_ FTP
+$high_priority_source set packetSize_ 1500
+$high_priority_source set rate_ 2Mbps
 
-# Set different priority levels for traffic
-$ns set-priority $node0 $node1 0 1 2
+# Set up low-priority traffic source and sink
+set low_priority_source [new Application/Traffic/FTP]
+$low_priority_source attach-agent $low_priority_host
+set low_priority_sink [new Agent/TCPSink]
+$ns attach-agent $low_priority_host $low_priority_sink
+$ns connect $low_priority_source $low_priority_sink
+$low_priority_source set type_ FTP
+$low_priority_source set packetSize_ 1500
+$low_priority_source set rate_ 1Mbps
 
-# Create traffic flows with different priorities
-set tcp0 [new Agent/TCP]
-$ns attach-agent $node0 $tcp0
+# Implement QoS by setting different queue sizes
+$ns queue-limit $high_priority_host $router 5
+$ns queue-limit $low_priority_host $router 10
 
-set tcp1 [new Agent/TCP]
-$ns attach-agent $node1 $tcp1
+# Schedule events
+$ns at 0.1 "$high_priority_source start"
+$ns at 0.1 "$low_priority_source start"
+$ns at 5.0 "$ns stop"
 
-set ftp0 [new Application/FTP]
-$ftp0 attach-agent $tcp0
-$ns connect $ftp0 $node1 0
-
-set ftp1 [new Application/FTP]
-$ftp1 attach-agent $tcp1
-$ns connect $ftp1 $node0 0
-
-# Schedule events and run the simulation
-$ns at 0.1 "$ftp0 start"
-$ns at 0.2 "$ftp1 start"
-
+# Run the simulation
 $ns run
